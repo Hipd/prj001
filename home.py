05e48c33531a0ecedcc5ffaf4b1f54843ba43918
hipd <nohziwer@163.com>
2018-05-08 09:38:32 +0800

    2

--- home.py
+++ home.py
@@ -5,6 +5,18 @@ import os
 
 help(ord)
 
+
 def test():
     pass
+
+
+class helpme():
+    def __init__(self):
+        self.color = "black"
+        print(self.color)
+
+
+def my_color(helpme):
+    pass
+
 
